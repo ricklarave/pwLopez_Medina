d3a30d90f779d750ae0329f73be45df6211fff09
ricklarave <ricardo.mera193@gmail.com>
2023-01-30 16:04:45 -0500

    corrections

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,16 +1,16 @@
 import Product from "../models/Product"
 
 export const createProduct = async (req, res) => {
-    const {name, category, price, imgURL} = req.body
+    const { name, category, price, imgURL } = req.body
 
-    const newProduct = new Product({name: category, price, imgURL});
-    
+    const newProduct = new Product({ name, category, price, imgURL });
+
     const productSaved = await newProduct.save()
 
     res.status(201).json(productSaved)
 }
 
-export const getProducts = async(req, res) => {
+export const getProducts = async (req, res) => {
     const products = await Prodcut.find();
     res.json(products)
 }
@@ -21,9 +21,13 @@ export const getProductById = async (req, res) => {
 }
 
 export const updateProductById = async (req, res) => {
-    const updateProduct = await Product.findByIdAndUpdate(req.params.productId, req.body,{
-        new: true
-    })
+    const updateProduct = await Product.findByIdAndUpdate(
+        req.params.productId,
+        req.body,
+        {
+            new: true
+        }
+    )
     res.status(204).json(updateProduct)
 }
 
