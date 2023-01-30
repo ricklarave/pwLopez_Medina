2e61960969ab9de31776d667d8cef8bd2c9ab6dd
ricklarave <ricardo.mera193@gmail.com>
2023-01-30 15:04:13 -0500

    checkDuplicateUsernmaeOrEmail

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 const router = Router()
 
 import * as authCtrl from '../controllers/auth.controller'
+import {verifySignup} from '../middlewares'
 
-router.post('/singup', authCtrl.singUp)
+router.post('/singup', [verifySignup.checkDuplicateUsernmaeOrEmail, verifySignup.checkRolesExisted] authCtrl.singUp)
 router.post('/singin', authCtrl.singin)
 
 export default router;
