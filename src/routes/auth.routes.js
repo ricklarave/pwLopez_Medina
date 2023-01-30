327c3cd52fe139a936ae01fbe2408924a098ff59
ricklarave <ricardo.mera193@gmail.com>
2023-01-30 16:16:08 -0500

    correction

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,7 @@ const router = Router()
 import * as authCtrl from '../controllers/auth.controller'
 import {verifySignup} from '../middlewares'
 
-router.post('/singup', [verifySignup.checkDuplicateUsernmaeOrEmail, verifySignup.checkRolesExisted] authCtrl.singUp)
+router.post('/singup', [verifySignup.checkDuplicateUsernmaeOrEmail, verifySignup.checkRolesExisted], authCtrl.singUp)
 router.post('/singin', authCtrl.singin)
 
 export default router;
