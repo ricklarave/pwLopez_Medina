4747a8410d44bc089e028c09ebbb6af60da7f5ab
ricklarave <ricardo.mera193@gmail.com>
2023-01-30 14:46:40 -0500

    user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,12 @@
 import { Router } from "express";
 const router = Router()
 
+import * as userCtrl from '../controllers/user.controller'
+import {authJwt} from '../middlewares'
+
+router.post('/', [
+    authJwt.verifyToken,
+    authJwt.isAdmin
+],userCtrl.createUser)
+
 export default router;
